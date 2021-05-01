f72f50809b2e0d63e82fea6eb1e8a1b4acc2101a
George <71434803+george-swift@users.noreply.github.com>
2021-05-01 12:30:14 +0100

    Add bootstrap classes to array

--- script.js
+++ script.js
@@ -7,3 +7,10 @@ const author = form.querySelector('#book-author');
 const title = form.querySelector('#book-title');
 const pages = form.querySelector('#pages');
 const read = form.querySelector('#read-book');
+
+const shelf = document.querySelector('.shelf');
+
+const bookClasses = ['col-md-6', 'offset-md-3', 'text-center', 'bg-light', 'm-2', 'p-4', 'new-book'];
+const readBtnClasses = ['btn', 'btn-outline-success', 'mb-3'];
+const notReadClasses = ['btn', 'btn-outline-secondary', 'mb-3'];
+const removeBtnClasses = ['btn', 'btn-warning', 'w-100'];

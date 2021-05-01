6f42902a6db5a9446d901ea3b079398f15d49ed7
George <71434803+george-swift@users.noreply.github.com>
2021-05-01 12:29:36 +0100

    Add cached elements

---
+++ script.js
@@ -0,0 +1,9 @@
+const overlay = document.querySelector('.overlay');
+const showForm = document.querySelector('.add-btn');
+const closeForm = document.querySelector('.close');
+
+const form = document.querySelector('#book-form');
+const author = form.querySelector('#book-author');
+const title = form.querySelector('#book-title');
+const pages = form.querySelector('#pages');
+const read = form.querySelector('#read-book');
